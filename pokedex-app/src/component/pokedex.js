d9bdabffbd4e06c9640df2e2dfa6585e39a65660
HaoyuanDu <jasonduuuu@hotmail.com>
2021-05-08 18:58:35 -0400

    1.1

--- pokedex-app/src/component/pokedex.js
+++ pokedex-app/src/component/pokedex.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import TableContainer from './tableContainer';
-import { Container, Button } from "reactstrap"
-import "bootstrap/dist/css/bootstrap.min.css"
+//import { Container, Button } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { img, Container, Button, Row, Col } from "reactstrap"
+//import { SelectColumnFilter } from './filters';
+//import Pokemon from './pokemon';
 
 const Pokedex = () => {
   const [data, setData] = useState([]);
@@ -9,64 +12,188 @@ const Pokedex = () => {
     const doFetch = async () => {
       const response = await fetch('http://localhost:3001/pokedex');
       const body = await response.json();
-      console.log(body);
+      //console.log(body);
       setData(body);
     };
     doFetch();
   }, []); 
-
+  const renderRowSubComponent = row => {
+    const {
+        id,
+        name: { english, chinese, japanese, french },
+        type,
+        base: { HP, Attack, Defense, Speed },
+        
+    } = row.original
+    return (
+      <Container style={{ width: "auto", margin: "0 auto" }}>
+          <Row className="justify-content-md-center">
+            <Col className="justify-content-md-center" xs lg="12">
+              <strong>{`${id} ${english}`} </strong>
+            </Col>
+          </Row>
+          <Row className="justify-content-md-center">
+            <Col xs lg="5">
+                <img width="100%" src="http://localhost:3001/images/001.png"  alt="Card cap"/>
+            </Col>
+            <Col xs lg="3">
+                <strong>French Name</strong> <br />
+                <strong>Chinese Name</strong> <br />
+                <strong>japanese Name</strong><br />
+                <strong>Type:</strong><br />
+                <strong>HP</strong> <br />
+                <strong>Attack</strong> <br />
+                <strong>Defense</strong> <br />
+                <strong>speed</strong> <br />
+                <strong>Attack</strong> <br />
+                <strong>Defense</strong> <br />
+            </Col>
+            <Col xs lg="3">
+                : {french} <br />
+                : {chinese} <br />
+                : {japanese} <br />
+                <Button>{type}</Button><br />
+                : {HP} <br />
+                : {Attack} <br />
+                : {Defense} <br />
+                : {Speed} <br />
+                : {Attack} <br />
+                : {Defense} <br />
+            </Col>
+          </Row>
+          
+        {/* <CardImg left src="http://localhost:3001/images/001.png" rounded alt="Card image cap" />
+        <CardBody>
+          <CardTitle>
+            <strong>{`${id} ${english}`} </strong>
+          </CardTitle>
+          <CardText>
+            <strong>French Name</strong>: {french} <br />
+            <strong>Chinese Name</strong>: {chinese} <br />
+            <strong>japanese Name</strong>: {japanese} <br />
+            <strong>Base data:</strong> {`${HP} ${Attack} - ${Defense} - ${speed}`}
+          </CardText>
+        </CardBody> */}
+      </Container>
+    )
+  }
   const columns = useMemo(
     () => [
-      {
-        Header: "ID",
-        accessor: "id",
-      },
-      {
-        Header: "Name",
-        accessor: "name.english",
-      },
-      {
-        Header: "HP",
-        accessor: "base.HP",
-      },
-      {
-        Header: "Attack",
-        accessor: "base.Attack",
-      },
-      {
-        Header: "Defense",
-        accessor: "base.Defense",
-      },
-      {
-        Header: "Sp. Attack",
-        accessor: (value) => value.base['Sp. Attack'],
-      },
-      {
-        Header: "Sp. Defense",
-        accessor: (value) => value.base['Sp. Defense'],
-      },
-      {
-        Header: "Speed",
-        accessor: "base.Speed",
-      },
-      {
-        Header: 'Type',
-        accessor: (values) => {
-            const first = values.type[0];
-            const second = values.type[1];
-            return first + '/' + second;
+        {
+            Header: () => null,
+            id: 'expander', // 'id' is required
+            Cell: ({ row }) => (
+              <span {...row.getToggleRowExpandedProps()}>
+                {row.isExpanded ? '👇' : '👉'}
+              </span>
+            ),
+          },
+        {
+            Header: "ID",
+            accessor: "id",
+            disableFilters: true,
+
+        // Cell:({cell}) => {
+        //     const { value } = cell;
+        //     console.log("value:",value);
+        //     const pickImg = (value) => {
+        //         //value = 000 + value;
+        //         let first = "/sprites/" + value + "MS.png"; 
+        //         let src = baseUrl + first;
+        //         console.log(src);
+        //         return (
+        //             <div> <img width="100%" src={baseUrl + first} alt={value} /></div>
+        //         )
+        //       };
+
+        //     return (
+        //     <div style={{ textAlign: 'center', fontSize: 18 }}>
+        //         {pickImg(value)}
+        //     </div>
+        //     );
+        
+        // }
+
         },
-        Cell:({cell}) => {
-            const { value } = cell;
-        }
-    },
+    //   {
+    //     Header: "ID",
+    //     accessor: "id",
+    //   },
+        {
+            Header: "Name",
+            accessor: "name.english",
+        },
+        {
+            Header: "HP",
+            accessor: "base.HP",
+            disableFilters: true,
+        },
+        {
+            Header: "Attack",
+            accessor: "base.Attack",
+            disableFilters: true,
+        },
+        {
+            Header: "Defense",
+            accessor: "base.Defense",
+            disableFilters: true,
+        },
+        {
+            Header: "Sp. Attack",
+            accessor: (value) => value.base['Sp. Attack'],
+            disableFilters: true,
+        },
+        {
+            Header: "Sp. Defense",
+            accessor: (value) => value.base['Sp. Defense'],
+            disableFilters: true
+        },
+        {
+            Header: "Speed",
+            accessor: "base.Speed",
+            disableFilters: true,
+        },
+        {
+            Header: 'Type',
+            accessor: (values) => {
+                const first = values.type[0];
+                const second = values.type[1];
+                if(second){
+                    return first + '/' + second;
+                //     return (
+                //         <p><Button>{first}</Button><Button>{second}</Button></p>
+                //     )
+                }
+                // return (
+                //     <p><Button>{first}</Button></p>
+                // )
+                return first;
+            },
+            Cell:({cell}) => {
+                const { value } = cell;
+                //console.log("value:",value);
+                const pickEmoji = (value) => {
+                    let first = value; 
+                    return (
+                        <p><Button>{first}</Button></p>
+                    )
+                };
+
+                return (
+                <div style={{ textAlign: 'center', fontSize: 18 }}>
+                    {pickEmoji(value)}
+                </div>
+                );
+            
+            }
+        },
     ],
     []
   );
 
   return (
     <Container style={{ marginTop: 100 }}>
-      <TableContainer columns={columns} data={data} />;
+      <TableContainer columns={columns} data={data} renderRowSubComponent={renderRowSubComponent}/>;
     </Container>
     )
 };
--- pokedex-app/src/component/tableContainer.js
+++ pokedex-app/src/component/tableContainer.js
@@ -1,27 +1,44 @@
-import React from 'react';
-import { useTable } from 'react-table';
+import React, { Fragment } from 'react';
+import { useTable, useSortBy, useFilters, useExpanded } from 'react-table';
 import { Table } from 'reactstrap';
+import { Filter, DefaultColumnFilter } from './filters';
 
-
-const TableContainer = ({ columns, data }) => {
+const TableContainer = ({ columns, data, renderRowSubComponent }) => {
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
     rows,
     prepareRow,
-  } = useTable({
-    columns,
-    data,
-  });
-  console.log(headerGroups);
+    visibleColumns,
+  } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn: { Filter: DefaultColumnFilter },
+    },
+    useFilters,
+    useSortBy,
+    useExpanded
+  );
+
+  const generateSortingIndicator = (column) => {
+    return column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : '';
+  };
+
   return (
     <Table bordered hover {...getTableProps()}>
       <thead>
         {headerGroups.map((headerGroup) => (
           <tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              <th {...column.getHeaderProps()}>
+                <div {...column.getSortByToggleProps()}>
+                  {column.render('Header')}
+                  {generateSortingIndicator(column)}
+                </div>
+                <Filter column={column} />
+              </th>
             ))}
           </tr>
         ))}
@@ -31,11 +48,22 @@ const TableContainer = ({ columns, data }) => {
         {rows.map((row) => {
           prepareRow(row);
           return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
-              })}
-            </tr>
+            <Fragment key={row.getRowProps().key}>
+              <tr>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  );
+                })}
+              </tr>
+              {row.isExpanded && (
+                <tr>
+                  <td colSpan={visibleColumns.length}>
+                    {renderRowSubComponent(row)}
+                  </td>
+                </tr>
+              )}
+            </Fragment>
           );
         })}
       </tbody>
